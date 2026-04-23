5a78f533e9a4f63fc9eb69c3de2910ce5741b2e6
Marius 't Hart <thartbm@gmail.com>
2026-04-23 14:24:27 -0400

    move the hi fusion stim off to the side where the other stimuli are not

--- HVperceptionBS.py
+++ HVperceptionBS.py
@@ -134,6 +134,11 @@ def doHVperceptionTask(ID=None, hemifield=None, location=None):
     hiFusion = setup['fusion']['hi']
     loFusion = setup['fusion']['lo']
 
+    if hemifield == 'left':
+        hiFusion.pos = [15, 0]
+    if hemifield == 'right':
+        hiFusion.pos = [-15, 0]
+
     blindspot = setup['blindspotmarkers'][hemifield]
     # print(blindspot.fillColor)
     
